f68692381aeed665773f38c9fd3852997be20334
NestorLiao <gtkndcbfhr@gmail.com>
2024-04-11 22:51:39 +0800

    finish quick start

---
+++ add/target/debug/deps/add-13c30c33f792a2f8.d
@@ -0,0 +1,9 @@
+/home/nestor/resin/waged/waged/add/target/debug/deps/libadd-13c30c33f792a2f8.rmeta: src/lib.rs Cargo.toml
+
+/home/nestor/resin/waged/waged/add/target/debug/deps/add-13c30c33f792a2f8.d: src/lib.rs Cargo.toml
+
+src/lib.rs:
+Cargo.toml:
+
+# env-dep:CLIPPY_ARGS=
+# env-dep:CLIPPY_CONF_DIR
---
+++ add/target/debug/deps/add-59dccc89d52a711b.d
@@ -0,0 +1,9 @@
+/home/nestor/resin/waged/waged/add/target/debug/deps/libadd-59dccc89d52a711b.rmeta: src/lib.rs Cargo.toml
+
+/home/nestor/resin/waged/waged/add/target/debug/deps/add-59dccc89d52a711b.d: src/lib.rs Cargo.toml
+
+src/lib.rs:
+Cargo.toml:
+
+# env-dep:CLIPPY_ARGS=
+# env-dep:CLIPPY_CONF_DIR
